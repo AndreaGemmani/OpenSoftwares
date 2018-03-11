e5c921afbc6c17536c16b1ffe85f654592d65dd1
Andrea Gemmani <32385179+Gimmmy97@users.noreply.github.com>
2018-03-11 23:51:00 +0100

    aggiunto handler cartelle

--- challenge/Scacchi/caricaTxtr.js
+++ challenge/Scacchi/caricaTxtr.js
@@ -1,17 +1,28 @@
 caricatorePezzi = function() {
 
+    // PN = loadImage('src=../../pezziScacchi/pedoneNero.ico'); 
+    // PB = loadImage('src=../../pedoneBianco.ico');
+    // AN = loadImage("src=../../alfiereNero.ico");
+    // AB = loadImage("src=../../alfiereBianco.ico");
+    // TN = loadImage("src=../../torreNera1.ico"); 
+    // TB = loadImage("src=../../torreBianca.ico");
+    // CN = loadImage("src=../../cavalloNero1.ico");
+    // CB = loadImage("src=../../cavalloBianco.ico");
+    // RN = loadImage("src=../../reNero.ico"); 
+    // RB = loadImage("src=../../reBianco.ico");
+    // RgN = loadImage("src=../../reginaNera.ico");
+    // RgB = loadImage("src=../../reginaBianca.ico");
+
     PN = loadImage('src=../../pezziScacchi/pedoneNero.ico'); 
-    PB = loadImage('src=../../pedoneBianco.ico');
-    AN = loadImage("src=../../alfiereNero.ico");
-    AB = loadImage("src=../../alfiereBianco.ico");
-    TN = loadImage("src=../../torreNera1.ico"); 
-    TB = loadImage("src=../../torreBianca.ico");
-    CN = loadImage("src=../../cavalloNero1.ico");
-    CB = loadImage("src=../../cavalloBianco.ico");
-    RN = loadImage("src=../../reNero.ico"); 
-    RB = loadImage("src=../../reBianco.ico");
-    RgN = loadImage("src=../../reginaNera.ico");
-    RgB = loadImage("src=../../reginaBianca.ico");
-
-
+    PB = loadImage('src=../../pezziScacchi/pedoneBianco.ico');
+    AN = loadImage("src=../../pezziScacchi/alfiereNero.ico");
+    AB = loadImage("src=../../pezziScacchi/alfiereBianco.ico");
+    TN = loadImage("src=../../pezziScacchi/torreNera1.ico"); 
+    TB = loadImage("src=../../pezziScacchi/torreBianca.ico");
+    CN = loadImage("src=../../pezziScacchi/cavalloNero1.ico");
+    CB = loadImage("src=../../pezziScacchi/cavalloBianco.ico");
+    RN = loadImage("src=../../pezziScacchi/reNero.ico"); 
+    RB = loadImage("src=../../pezziScacchi/reBianco.ico");
+    RgN = loadImage("src=../../pezziScacchi/reginaNera.ico");
+    RgB = loadImage("src=../../pezziScacchi/reginaBianca.ico");
 }
