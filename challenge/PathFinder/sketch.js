a87034e7c27e8f22a0b25871244c465c8a0a0b40
Andrea Gemmani <32385179+Gimmmy97@users.noreply.github.com>
2018-04-18 00:10:16 +0200

    aggiunta fine e counter

--- challenge/PathFinder/sketch.js
+++ challenge/PathFinder/sketch.js
@@ -1,16 +1,16 @@
-// Andrea Gemmani 17/4/2018
+// Andrea Gemmani 17/4/2018, fruible under MIT license
 // GitHub Gimmmy97	https://github.com/Gimmmy97
 
 var labirinto;
 
-// var X = 30; // 60 fpps
+// var X = 30; // 60 fps
 // var Y = 20;
 
-var X = 50; // 30 fps (ottimo rapporto dimensioni/fps)
-var Y = 30;
+// var X = 50; // 30 fps (ottimo rapporto dimensioni/fps)
+// var Y = 30;
 
-// var X = 60; // 20 fps
-// var Y = 40;
+var X = 60; // 20 fps (labirinto abbastanza grande, buone prestazioni)
+var Y = 40;
 
 // var X = 80; // 15 fps
 // var Y = 50;
@@ -18,21 +18,29 @@ var Y = 30;
 // var X = 100; // 12 fps
 // var Y = 50;
 
+// var X = 150; // 7 fps, molto orizzontale
+// var Y = 40;
+
 // var X = 250; // 2 fps
 // var Y = 150;
 
-
-var perc = 0.2;		// 85/100 risolve
+// var perc = 0.2;	// 85/100 risolve
 // var perc = 0.3;	// 65/100 risolve
+var perc = 0.33;	// 65/100 risolve
 // var perc = 0.35;	// 45/100 risolve
 // var perc = 0.4;	// non ancora riuscito 
+// var perc = 0.45;	// impossibile probabilmente
+// var perc = 0.5;	// impossibile^2, ottimo per debug iniziale veloce
+// var perc = 0.8;	// impossibile^10, quasi tutto nero
 
 var m = 0;
 var n = 0;
 
 var dim = Math.round(800/X);
+
 var arrAttorno = [];
 var quale;
+var sommina = 0;
 
 function setup() {
 
@@ -51,7 +59,7 @@ function setup() {
 
 }
 
-draw = function() { // 0.3 arrivo, 0.7 partenza, 0.5 strada, 1 passsaggio, 0 muro
+draw = function() {
 // 0 muro, 0.1 vc, 0.15-0.85 passato, 0.9 libero, 1 arrivo
 // partenza 0.15
 	
@@ -61,6 +69,31 @@ draw = function() { // 0.3 arrivo, 0.7 partenza, 0.5 strada, 1 passsaggio, 0 mur
 	noStroke();
 	rectMode(CENTER);
 
+	// let risultato = trovaStrada();
+
+	push();
+		textSize(width/80);
+		textAlign(RIGHT,CENTER);
+		switch( trovaStrada() ) {
+			case 1:
+			case true:
+				fill(0,255,0);
+				text("Vittoria!",width-dim/4,height*11/20);
+				noLoop();
+				break;
+			case -1:
+				fill(255,0,0);
+				text("Impossibile!",width-dim/4,height*11/20);
+				noLoop();
+				break;
+			case 0:
+			case false:
+				fill(120);
+				text("In corso...",width-dim/4,height*11/20);
+				break;
+			default: break;
+		}
+	pop();
 
 	for(let i = 0; i < Y; i++) {
 		for(let k = 0; k < X; k++) {
@@ -75,7 +108,7 @@ draw = function() { // 0.3 arrivo, 0.7 partenza, 0.5 strada, 1 passsaggio, 0 mur
 		}
 	}
 
-	if(trovaStrada() == 1) noLoop();
+	// if(trovaStrada() == 1) noLoop();
 
 	push();
 	textSize(width/80);
@@ -86,21 +119,19 @@ draw = function() { // 0.3 arrivo, 0.7 partenza, 0.5 strada, 1 passsaggio, 0 mur
 	text(round(frameRate()) + " fps",width-dim/4,height*5/20);
 	text(X + " / " + Y,width-dim/4,height*7/20);
 	text(perc*100 + "% muri",width-dim/4,height*9/20);
-
 	pop();
 
 }
 
 var trovaStrada = function() {
 	labirinto[m][n] -= 0.05;
-	if(compara(m,n)) return 1;
-	return 0;
+	return compara(m,n);
 }
 
 var compara = function(j,k) {
 	arrAttorno = [];
 	let massimino = 1;
-	let sommina = 0;
+	sommina = 0;
 
 	if(k < X-1) arrAttorno[0] = (labirinto[j][k+1]);
 	else arrAttorno[0] = -0.01;
@@ -111,19 +142,13 @@ var compara = function(j,k) {
 	if(j > 0 ) arrAttorno[3] = (labirinto[j-1][k]);
 	else arrAttorno[3] = -0.01;
 
-	// let quale = indexOf(max(arrAttorno));
-	// quale = arrAttorno.indexOf(min(arrAttorno));
-
 	massimino = max(arrAttorno);
 	sommina = arrAttorno[0] + arrAttorno[1] + arrAttorno[2] + arrAttorno[3];
+	sommina = round(sommina*100)/100;
 	if(sommina <= 0.8) labirinto[m][n] = 0.1; // vicolo cieco da vicoli ciechi 
+	if(sommina <= 0.3) return (-1); // forse da mettere 0.4 ma non sono sicuro, potrebbe buggare
 	quale = arrAttorno.indexOf(massimino);
 	// quale = arrAttorno.indexOf(max(arrAttorno));
-
-	// m = quale[j];
-	// n = quale[k];
-	// m = arrAttorno[quale];
-	// n = arrAttorno[quale];
 
 	switch(quale) {
 		case 0: k++; break;
